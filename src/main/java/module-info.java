4206fae300ae48eec28a253f50ceddcac60299a9
CordlessCoder <roman@vm.net.ua>
2025-11-05 17:27:20 +0000

    Get JavaFX kind-of working

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module Zork {
+    exports org.example to javafx.graphics;
+    requires com.fasterxml.jackson.annotation;
+    requires io.github.classgraph;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires tools.jackson.databind;
+}
